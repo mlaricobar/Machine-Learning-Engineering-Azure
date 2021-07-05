47ce3723ab009aa1f9cb6007b72e30b71b0bcf23
Michael Larico <mlaricobar@gmail.com>
2021-07-04 20:37:00 -0500

    capstone project update

---
+++ 03. Capstone - Azure Machine Learning Engineer/score.py
@@ -0,0 +1,21 @@
+import pickle
+import json
+import pandas as pd
+import joblib
+from azureml.core.model import Model
+
+def init():
+    global model
+    
+    model_path = Model.get_model_path('default_automl_model')
+    model = joblib.load(model_path)
+
+
+def run(data):
+    try:
+        data = pd.DataFrame(json.loads(data)['data'])
+        result = model.predict(data)
+        return result.tolist()
+    except Exception as e:
+        error = str(e)
+        return error
